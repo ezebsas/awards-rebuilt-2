90cdbd3384dc2a6bc28d2c8bb9a00b547afdecda
Unknown <emollinedo@untref.edu.ar>
2020-10-08 20:57:39 -0300

    Layout, Header animado y seleccion de tema que se guarda en navegador

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,39 @@ import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+//styled components
+import { createGlobalStyle, ThemeProvider} from "styled-components"
+import {normalize} from "styled-normalize"
+
+//Components
+import Header from "./header"
+
+//Context
+import { useGlobalStateContext } from '../context/globalContext'
+
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  *{
+    text-decoration: none;
+  }
+
+  html{
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialised;
+    font-size: 16px;
+  }
+
+  body{
+    font-family: sans-serif;
+    background: ${ props => props.theme.background};
+    overscroll-behavior: none;
+    overflow-x: hidden;
+  }
+
+
+`
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -13,7 +46,27 @@ const Layout = ({ children }) => {
     }
   `)
 
-  return <main>{children}</main>
+  const darkTheme = {
+    background: "#000",
+    text: "#fff",
+    red: '#ea291e',
+  }
+
+  const lightTheme = {
+    background: "#fff",
+    text: "#000",
+    red: '#ea291e',
+  }
+
+  const { currentTheme } = useGlobalStateContext()
+
+  return (
+    <ThemeProvider theme={currentTheme === 'dark' ? darkTheme : lightTheme}>
+      <GlobalStyle />
+      <Header />
+      <main>{children}</main>
+    </ThemeProvider>
+  )
 }
 
 Layout.propTypes = {
